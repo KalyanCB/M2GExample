89d1d45f36077609f7f6eef01ef72d831cb02b7a
Kalyan Chakravarthy Battula <kalyancb@Kalyans-Laptop.local>
2025-06-06 18:53:57 +0530

    Initial commit - Spring Boot Maven microservice

---
+++ src/main/java/org/example/M2GExampleApplication.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M2GExampleApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(M2GExampleApplication.class, args);
+    }
+}
